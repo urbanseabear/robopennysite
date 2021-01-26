0a9934047d36f725ab50a1192e89ad6f62ef0cc6
urbanseabear <calvin.rudy@gmail.com>
2021-01-25 19:55:42 -0500

    added to about section

--- pages/about.tsx
+++ pages/about.tsx
@@ -27,7 +27,7 @@ function About() {
             Hi! My name is Calvin and you can find me most nights somewhere on
             the Goblin server on Crystal in FFXIV. I made this bot in order to
             work on my dev skills and give our FC discord server some unique
-            abilities.
+            abilities. If you've been directed here as a portfolio link, click <button>here</button> to view my resume.
           </i>
         </p>
         <h3
@@ -40,7 +40,11 @@ function About() {
             }
           }}
         >
-          PENNY URA <span style={{ fontSize: "1rem" }}>&#9660;</span>
+          PENNY URA  
+          {showPics === "hidden"
+              ? <span style={{ marginLeft: "8px", fontSize: "1rem" }}>&#9660;</span>
+              : <span style={{ marginLeft: "8px",fontSize: "1rem" }}>&#9650;</span>
+          }
         </h3>
         <img
           src="https://img2.finalfantasyxiv.com/f/9c0b4a932340579824dd05b0db0dc0d5_8e81e2940511f7152ba4462fe53e35b8fl0_640x873.jpg?1611084345&_ga=2.91680218.1124699889.1610844458-424753855.1557808539"
